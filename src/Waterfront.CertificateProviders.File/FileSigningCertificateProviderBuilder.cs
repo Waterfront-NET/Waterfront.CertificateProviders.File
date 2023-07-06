bcdabba962f0d070647277cfb27fbe5b76eec33a
2CHEVSKII <prsroman3@gmail.com>
2023-07-06 10:13:46 +0300

    ...

---
+++ src/Waterfront.CertificateProviders.File/FileSigningCertificateProviderBuilder.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Waterfront.Extensions.DependencyInjection;
+
+namespace Waterfront.CertificateProviders.File;
+
+public class FileSigningCertificateProviderBuilder
+{
+    private readonly IWaterfrontBuilder _waterfrontBuilder;
+
+    public FileSigningCertificateProviderBuilder(IWaterfrontBuilder waterfrontBuilder)
+    {
+        _waterfrontBuilder = waterfrontBuilder;
+    }
+
+    public FileSigningCertificateProviderBuilder WithServiceCertificate(
+        string servicePattern,
+        string optionsName,
+        Action<FileSigningCertificateOptions> configureOptions
+    )
+    {
+        return AddCertificateOptions(optionsName, configureOptions)
+        .SetServiceOptions(servicePattern, optionsName);
+    }
+
+    public FileSigningCertificateProviderBuilder AddCertificateOptions(
+        string name,
+        Action<FileSigningCertificateOptions> configureOptions
+    )
+    {
+        _waterfrontBuilder.Services.Configure(name, configureOptions);
+    }
+
+    public FileSigningCertificateProviderBuilder SetServiceOptions(
+        string servicePattern,
+        string name
+    )
+    {
+        _waterfrontBuilder.Services.Configure<FileSigningCertificateProviderOptions>(
+            options => options[servicePattern] = name
+        );
+        return this;
+    }
+}
---
+++ src/Waterfront.CertificateProviders.File/FileSigningCertificateProvidersWaterfrontBuilderExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Waterfront.Extensions.DependencyInjection;
+
+namespace Waterfront.CertificateProviders.File;
+
+public static class FileSigningCertificateProvidersWaterfrontBuilderExtensions
+{
+    public static IWaterfrontBuilder WithFileSigningCertificateProvider(
+        this IWaterfrontBuilder builder,
+        Action<FileSigningCertificateProviderBuilder> configureCertificateProvider
+    )
+    {
+        var certificateProviderBuilder = new FileSigningCertificateProviderBuilder(builder);
+        configureCertificateProvider(certificateProviderBuilder);
+        return builder;
+    }
+}
